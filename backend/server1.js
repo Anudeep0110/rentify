71a26f9b0cd49a302951ac840b76ee72e38be87f
SAI SREE <gnanasaisreemandarapu@gmail.com>
2024-05-19 20:07:37 +0530

    added login

--- backend/server1.js
+++ backend/server1.js
@@ -2,10 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const connection = require('./Connection')
 const col_users = require('./databases/login');
-const col_properties = require('./databases/properties');
+const col_properties = require('./databases/property');
 
 const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+app.post('/login',(req,res) => {
+    console.log(req.body);
+    col_properties.find({})
+    .then(res2 => {
+        console.log(res2);
+        if(res2.length != 0){
+            res.json(true)
+        }else{
+            res.json(false)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+}) 
+module.exports = app 
